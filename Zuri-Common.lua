64b7cd874dce59c7cd85bef72e478d130c63451f
kassisaf <kassisaf@gmail.com>
2023-05-13 10:38:40 -0700

    Rename _equip to safe_equip

--- Zuri-Common.lua
+++ Zuri-Common.lua
@@ -25,7 +25,8 @@ end
 
 -- Custom equip function that respects items defined in `lockables`
 local lockables_set = to_set(lockables)
-function _equip(gearset)
+function safe_equip(gearset)
+    -- For each item in the set, check if player has a lockable item in that slot before equipping
     for slot, item in pairs(gearset) do
         if lockables_set[player.equipment[slot]] then
             disable(slot)
@@ -59,18 +60,18 @@ end -- equip_idle_or_tp_set()
 function equip_idle_set()
     -- Conditional movespeed gear
     if string.find(world.zone, "Adoulin") and sets.idle.adoulin then
-        _equip(sets.idle, {body = "Councilor's Garb"})
+        safe_equip(sets.idle, {body = "Councilor's Garb"})
     elseif player.main_job_level == 99 then
-        _equip(sets.idle, {right_ring = "Shneddick Ring"})
+        safe_equip(sets.idle, {right_ring = "Shneddick Ring"})
     end
 end -- equip_idle_set()
 
 
 function equip_tp_set()
     if buffactive["Elvorseal"] and sets.DI then
-        _equip(sets.DI)
+        safe_equip(sets.DI)
     else
-        _equip(sets.TP)
+        safe_equip(sets.TP)
     end
 end -- equip_tp()
 
@@ -81,21 +82,21 @@ function precast(spell, position)
     -- Use WS-specific set if it exists, or fall back to generic WS set
     if spell.type == "WeaponSkill" then
         if sets.precast.WS[spell.english] then
-            _equip(sets.precast.WS[spell.english])
+            safe_equip(sets.precast.WS[spell.english])
         else
-            _equip(sets.precast.WS)
+            safe_equip(sets.precast.WS)
         end
     -- Use JA-specific set if it exists
     elseif spell.type == "JobAbility" and sets.precast.JA[spell.english] then
-        _equip(sets.precast.JA[spell.english])
+        safe_equip(sets.precast.JA[spell.english])
     elseif spell.type == "Ranged Attack" then
-        _equip(sets.precast.RA)
+        safe_equip(sets.precast.RA)
     -- Use spell-specific precast set if it exists
     elseif sets.precast[spell.english] then
-        _equip(sets.FC, sets.precast[spell.english])
+        safe_equip(sets.FC, sets.precast[spell.english])
     -- Use fastcast set for magic if nothing else matched
     elseif string.find(spell.type, "Magic") or spell.type == "BardSong" then
-        _equip(sets.FC)
+        safe_equip(sets.FC)
     end
 end -- precast()
 
@@ -103,10 +104,10 @@ end -- precast()
 function midcast(spell)
     -- Use spell-specific midcast set if it exists
     if sets.midcast[spell.english] then
-        _equip(sets.midcast[spell.english])
+        safe_equip(sets.midcast[spell.english])
     -- Use type-specific midcast set if it exists (i.e. elemental magic)
     elseif sets.midcast[spell.type] then
-        _equip(sets.midcast[spell.type])
+        safe_equip(sets.midcast[spell.type])
     -- Fall back to idle set if nothing else matched
     else
         equip_idle_or_tp_set()
